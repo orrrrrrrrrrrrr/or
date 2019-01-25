534a3ee3711bf80e678fc3d1662c34dc9c236fa1
or <oriri101010@gmail.com>
2019-01-25 22:14:25 +0900

    html file

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿window.onload = function( ev )
+{
+    const   ca = document.getElementById( 'main' );
+    const   g = ca.getContext( '2d' );
+    g.font = "80px monospace";
+    g.fillStyle = "#00ff00";
+    g.fillText( "こんにちは世界", 40, 80 );
+}
